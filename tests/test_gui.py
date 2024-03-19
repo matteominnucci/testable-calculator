1bc58fe58627159771518fb1411cab1ac1f86775
Matteo Minnucci <matteo.minnucci@hotmail.it>
2024-03-19 17:00:44 +0100

    add test for layouts

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -10,6 +10,7 @@ class CalculatorGUITestCase(unittest.TestCase):
 
     def press_button(self, button_text):
         self.app.find_button_by(button_text).trigger_action()
+        #actually pressing the button
 
     def assert_display(self, value):
         self.assertEqual(self.app.display.text, value)   
@@ -17,6 +18,8 @@ class CalculatorGUITestCase(unittest.TestCase):
     def tearDown(self):
         self.app.stop()
     
+    def assert_button_exists(self, button_text):
+        self.assertIsNotNone(self.app.find_button_by(button_text))
 
 class TestExpressions(CalculatorGUITestCase):
     def test_integer_expression(self):
@@ -36,3 +39,15 @@ class TestExpressions(CalculatorGUITestCase):
         self.assert_display("1.2+2")
         self.press_button("=")
         self.assert_display("3.2")
+
+class TestLayout(CalculatorGUITestCase):
+    
+    def test_display(self):
+        self.app = CalculatorApp()
+        self.app._run_prepare()
+    
+    def test_initial_expression(self):
+        for button_text in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "=", "C", "."]:
+            #with statement enables me to see each single case
+            with self.subTest(button=button_text):
+                self.assert_button_exists(button_text)
